6648def0e41e351f7f6ac2bbe0d42b90081251dc
wittenberg <sven.wittenberg@gmail.com>
2016-05-22 23:15:27 +0200

    anpassungen#

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+$jetzt = time();
 
 /*$memcache = new Memcached();
 $memcache->addserver('memcache-php', 11211);
@@ -17,16 +18,36 @@ var_dump($memcache->getStats());*/
 
 
 
-$db = new mysqli("mysql-connect", "root", 'test', 'testtable');
-$db->query('
-    inset into testtable
-    (
-      id int,
-      name VARCHAR(255)
-    );'
-);
+$db = new mysqli("mysql-connect", "root", 'test', 'testdb');
+$db->options(MYSQLI_OPT_CONNECT_TIMEOUT, 5);
+
+//$db->query("
+//    create table testtable (
+//      id int,
+//      name VARCHAR(255)
+//    );"
+//);
+
+//$db->query("
+//    insert into testtable (id, name)
+//    VALUES (1, 'Tim');
+//        insert into testtable (id, name)
+//    VALUES (2, 'Sven');
+//        insert into testtable (id, name)
+//    VALUES (3, 'Helge');
+//        insert into testtable (id, name)
+//    VALUES (4, 'Martin');
+//        insert into testtable (id, name)
+//    VALUES (5, 'Falk');
+//
+//");
 
 
+$result = $db->query("
+    select * from testtable;          
+    
+");
 
+var_dump('rows: ' . $result->num_rows . PHP_EOL);
 
-echo 'test';
+var_dump('Zeit: '. (time()-$jetzt) . PHP_EOL);
